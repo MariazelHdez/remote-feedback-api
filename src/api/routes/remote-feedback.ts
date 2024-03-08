8d5305850294808405bf6a2c31c5ab536db18313
Mariazel Hernández <mariazelhernandez@Mariazels-MacBook-Pro.local>
2024-03-08 12:51:48 -0700

    update

--- src/api/routes/remote-feedback.ts
+++ src/api/routes/remote-feedback.ts
@@ -3,7 +3,8 @@ import { body, param } from "express-validator";
 import * as dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
-
+const ejs = require('ejs');
+const fs = require('fs');
 const sanitizeHtml = require('sanitize-html');
 
 let path;
@@ -21,38 +22,80 @@ dotenv.config({ path: path });
 
 export const remoteFeedbackRouter = express.Router();
 
+
 remoteFeedbackRouter.post("/feedbackForm", async (req: Request, res: Response) => {
     try {
+
+         //Content from remote feedback webform
+         let  data = req.body;
+         let wasThisPageHelpful = sanitizeHtml( data.was_this_page_helpful);
+         var emailOption = '';
+         let emailComment = '';
+         if (wasThisPageHelpful === 'Yes') {
+             emailOption = 'How did this page help you?';
+             emailComment =  sanitizeHtml(data.how_did_this_page_help_you);
+         } else{
+             emailOption = 'How can we improve this page?';
+             emailComment =  sanitizeHtml(data.how_can_we_improve_this_page);
+        } 
+
         //Content from client
-        let feedbackContentSubject = sanitizeHtml(req.body.emailSubject);
-        let feedbackFormContent = sanitizeHtml(req.body.emailBody);
-        let emailDate = new Date();;
-        let pageUrl = sanitizeHtml(req.body.pageUrl)
+        let pageUrl = data.current_page_url;
+        let domain = data.domain;
+        let submission_timestamp = data.submission_timestamp;
+        let langcode = data.langcode;
 
-        const bodyContentFormatted =
-            `<p><strong>Submited on:</strong> ${emailDate.toLocaleString()}</p>
-        <p><strong>${feedbackContentSubject} :</strong> ${feedbackFormContent}</p> 
-        <p<strong>URL:</strong> <a href="${pageUrl}">${pageUrl}</a></p>`;
 
-        const sanitizedBody = sanitizeHtml(bodyContentFormatted)
+        // Load the content of the HTML template
+        const emailTemplate = fs.readFileSync('./template/feedbackEmail.ejs', 'utf-8');
+
+
+        // Data to personalize the email
+        const emailData = {
+            submitedOn: submission_timestamp,
+            site : domain,
+            lang: langcode,
+            emailLabel: emailOption,
+            emailContent: emailComment,
+            urlFrom : pageUrl
+        };
+
+        // Render the HTML template using EJS
+        const html = ejs.render(emailTemplate, emailData);
         const emailHost = process.env.SMTP_SERVER;
         const emailPort: string = process.env.SMTP_PORT!;
         const emailFrom = process.env.EMAIL_FROM;
         const nameFrom = process.env.NAME_FROM;
         const subject = process.env.EMAIL_SUBJECT;
+        const emailTo = process.env.EMAIL_TO;
         const selfSignedConfig = {
             host: emailHost,
             port: parseInt(emailPort)
         };
         var transporter = nodemailer.createTransport(selfSignedConfig);
-        const info = await transporter.sendMail({
-            from: nameFrom + ' ' + emailFrom,
-            to: process.env.EMAIL_TO,
+
+        // Configure the email
+        const mailOptions = {
+            from: emailFrom,
+            to: emailTo,
             subject: subject,
-            html: sanitizedBody,
+            html: html
+        };
+
+        const info = await transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log('Error sending email:', error);
+            } else {
+                res.send({ data: 'Sent' });
+            }
         });
-        res.send({ data: 'Sent' });
+
+
+
+
+
     } catch (error) {
         console.log(error);
     }
 });
+
