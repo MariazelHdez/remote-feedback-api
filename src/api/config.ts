3fe4500ef96094696f38def95868994b22abe4d1
Mariazel Hernández <mariazelhernandez@Mariazels-MacBook-Pro.local>
2024-03-08 16:13:38 -0700

    update

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,14 +1,18 @@
 import * as dotenv from "dotenv";
 
-const envPaths = {
-  "test": ".env.test",
-  "production": ".env",
-  "development": ".env.development"
-};
+let path;
+switch (process.env.NODE_ENV) {
+  case "test":
+    path = `.env.test`;
+    break;
+  case "production":
+    path = `.env`;
+    break;
+  default:
+    path = `.env.development`;
+}
 
-const path = envPaths[process.env.NODE_ENV] || ".env.development";
-
-dotenv.config({ path });
+dotenv.config({ path: path });
 
 export const API_PORT = parseInt(process.env.API_PORT || "3000");
 export const NODE_ENV = process.env.NODE_ENV;
