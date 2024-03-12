42c1c13c7dad2b4cd0033a4cab086ab894cad3b7
Mariazel Hernández <mariazelhernandez@Mariazels-MacBook-Pro.local>
2024-03-12 09:50:32 -0700

    remove dist

--- src/api/dist/utils/healthCheck.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.doHealthCheck = void 0;
-async function doHealthCheck(res) {
-    return res.status(200).send("HEALTHY");
-}
-exports.doHealthCheck = doHealthCheck;
